c3003e5ad455bed9053d0c8204e35eb69b28355d
Constitucion Abierta <you@example.com>
2016-06-22 22:00:48 -0400

    fix

--- constitucion_abierta/settings.py
+++ constitucion_abierta/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'constitucion'
+    'constitucion',
+    'mod_wsgi.server'
 ]
 
 MIDDLEWARE_CLASSES = [
