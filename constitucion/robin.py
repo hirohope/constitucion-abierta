f047b8ad7dd1a80767536337831e200dc056a3fd
cgarrido <you@example.com>
2016-06-27 16:49:31 -0400

    robin

--- constitucion/robin.py
+++ constitucion/robin.py
@@ -3,16 +3,13 @@
 #(nombre, mail)
 
 roundrobin = [
-#("Sebastián Ferrada", "scferradaa@gmail.com"),
-#("Camilo Garrido", "camilogarridogarcia@gmail.com"),
-#("Jorge Pérez", "jorgeperezrojas@gmail.com"),
-("Jorge Pérez 1", "jorgeperezrojas+1@gmail.com"),
-("Jorge Pérez 2", "jorgeperezrojas+2@gmail.com"),
-("Jorge Pérez 3", "jorgeperezrojas+3@gmail.com"),
-#("Claudio Gutiérrez", "cgutierr@dcc.uchile.cl"),
-#("Juan Reutter", "juan.reutter@gmail.com"),
-#("Adrian Soto", "assoto@uc.cl"),
-#("Bárbara Poblete", "barbara.poblete@gmail.com"),
-#("Lucas Cabello", "lmcabellog@gmail.com"),
-#("Mauricio Quezada", "waxkun@gmail.com"),
+("Sebastián Ferrada", "scferradaa@gmail.com"),
+("Camilo Garrido", "camilogarridogarcia@gmail.com"),
+("Jorge Pérez", "jorgeperezrojas@gmail.com"),
+("Claudio Gutiérrez", "cgutierr@dcc.uchile.cl"),
+("Juan Reutter", "juan.reutter@gmail.com"),
+("Adrian Soto", "assoto@uc.cl"),
+("Bárbara Poblete", "barbara.poblete@gmail.com"),
+("Lucas Cabello", "lmcabellog@gmail.com"),
+("Mauricio Quezada", "waxkun@gmail.com"),
 ]
