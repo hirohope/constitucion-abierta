5ab9bcb48cb978c8b88cf7dfd4ea881c4a07355c
Robinson Castro Fernández <r.a.c.f.28@gmail.com>
2016-07-10 22:41:35 -0400

    lemrewords sorted by freq

--- bin/export_lemrewords.py
+++ bin/export_lemrewords.py
@@ -1,18 +1,18 @@
 import codecs
 '''
 input:
-  'lemmas': {'lemma 1': {'r': 'representative1', 'w': {'word1', 'word2', ...}},
-             'lemma 2': {'r': 'representative2', 'w': {'word1', 'word2', ...}},
+  'lemmas': {'lemma 1': {'c': cnt1, 'r': 're1', 'w': {'word1', 'word2', ...}},
+             'lemma 2': {'c': cnt2, 'r': 're2', 'w': {'word1', 'word2', ...}},
             ...}
 
 output file:
   lines of the form "lemma    representative    word1,word2,..."
-  (sorted by lemma)
+  (sorted by freq)
 '''
 def export_lemrewords(data):
-    items = [(lemma, ld['r'], ld['w']) for lemma, ld in data['lemmas'].items()]
-    items.sort()
+    items = [(ld['c'], L, ld['r'], ld['w']) for L, ld in data['lemmas'].items()]
+    items.sort(reverse=True)
     with codecs.open(data['lemrewords.txt'], 'w', encoding='utf-8') as f:
-        for lemma, representative, words in items:
+        for count, lemma, representative, words in items:
             words = u','.join(sorted(list(words)))
             f.write(u"{}\t{}\t{}\n".format(lemma, representative, words))
